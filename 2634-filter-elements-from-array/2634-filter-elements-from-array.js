695334366a51f954a7ebe3d6e6493af5d666c1e4
Anshika jaiswal <94690313+Anshikaj69@users.noreply.github.com>
2023-12-26 22:51:54 +0530

    Time: 69 ms (5.02%), Space: 44.3 MB (8.13%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let newArr=[];
+    arr.forEach((element,index)=>{
+        if(fn(element,index) != false){
+            console.log(fn(element,index))
+           newArr.push(arr[index]);
+           }
+    })
+    return newArr;
+};
